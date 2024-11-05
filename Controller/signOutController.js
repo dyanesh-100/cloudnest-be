a5a1f0592595700fd95ddfa85b3296f42b1ba1b9
Dyanesh D <you@example.com>
2024-11-06 00:45:36 +0530

    Fixed. error in google signOut

--- Controller/signOutController.js
+++ Controller/signOutController.js
@@ -1,6 +1,8 @@
 const signOutController = (request, response) => {
     response.clearCookie('authToken'); 
     response.clearCookie('id_token');
+    response.clearCookie('access_token');
+    response.clearCookie('userProfile');
     response.status(200).send({ message: 'Successfully signed out' });
 
 };
