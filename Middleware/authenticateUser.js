d167b928152080454cf9767cdc54ae6ab576f623
Dyanesh D <you@example.com>
2024-11-06 00:34:00 +0530

    Fixed. error in google signIn

--- Middleware/authenticateUser.js
+++ Middleware/authenticateUser.js
@@ -6,29 +6,27 @@ const authenticateUser = async (request, response, next) => {
     const authToken = request.cookies.authToken;
     const googleIdToken = request.cookies.id_token;
 
-    
     if (!authToken && !googleIdToken) {
         return response.status(403).json({ message: 'Token missing or invalid' });
     }
 
     try {
         if (authToken) {
-            
             const decoded = jwt.verify(authToken, JWT_SECRET);
             request.userId = decoded.email;
+            
             next();
         } else if (googleIdToken) {
-            
             const googleUser = await verifyAccessToken(googleIdToken);
             if (!googleUser) {
                 return response.status(403).json({ message: 'Invalid Google token' });
             }
             request.userId = googleUser.email;
+           
             next();
         }
     } catch (error) {
         console.error('Authentication error:', error.message);
-        
         return response.status(403).json({ message: 'Invalid or expired token' });
     }
 };
