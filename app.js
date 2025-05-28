23329c863f0e5349e4597dfe1e38a357f5809baa
Dyanesh D <you@example.com>
2025-05-28 13:26:45 +0530

    Fixed. cors policy issue

--- app.js
+++ app.js
@@ -16,20 +16,9 @@ const app = express()
 
 const mongoose =require('mongoose')
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://cloudnest-fe.vercel.app',
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
+    origin: true, 
+    credentials: true
 }));
 
 app.use(express.json())
