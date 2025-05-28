a04d5190346ee69f75b234b11579287894442042
Dyanesh D <you@example.com>
2025-05-28 13:30:53 +0530

    Fixed. cors policy issue

--- Controller/signOutController.js
+++ Controller/signOutController.js
@@ -1,15 +1,9 @@
 const signOutController = (request, response) => {
-    const cookieOptions = {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'Lax', // Use 'None' if your frontend and backend are on different domains
-        path: '/',
-    };
-
-    response.clearCookie('authToken', cookieOptions);
-    response.clearCookie('id_token', cookieOptions);
-    response.clearCookie('access_token', cookieOptions);
-    response.clearCookie('userProfile', cookieOptions);
-
+    response.clearCookie('authToken');
+    response.clearCookie('id_token');
+    response.clearCookie('access_token');
+    response.clearCookie('userProfile');
     response.status(200).send({ message: 'Successfully signed out' });
 };
+
+module.exports = { signOutController };
